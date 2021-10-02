06808d45f08b32aabaabb21d61d949c9dac856e5
user <user@DESKTOP-MEDVOM0>
2021-10-02 11:46:28 -0500

    Agregar archivos de proyecto.

---
+++ PAESP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PAESP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

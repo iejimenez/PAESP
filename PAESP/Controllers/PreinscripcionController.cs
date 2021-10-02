71342d5373d495569f0a78cf7b884bff017230e2
luis jimenez <luisjimenezh7@gmail.com>
2021-10-02 13:51:05 -0500

    interfaces graficas de preinscripcion

---
+++ PAESP/Controllers/PreinscripcionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PAESP.Controllers
+{
+    public class PreinscripcionController : Controller
+    {
+        // GET: Preinscripcion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+      
+
+
+    }
+}
---
+++ PAESP/wwwroot/scripts/Preinscripcion.js
@@ -0,0 +1,71 @@
+﻿
+
+
+$(document).ready(function () {
+    window.InferfazPreinscripcion = new InferfazPreinscripcion()
+})
+
+
+
+
+class InferfazPreinscripcion {
+    constructor() {
+
+        this.clickHandlerGuardarPreinscritos = this.clickHandlerGuardarPreinscritos.bind(this)
+        this._initConstants()
+        this._initHtmlIds()
+        this._initEventBindings();
+        this._init()
+    }
+
+    _initHtmlIds() {
+        this.$inputTipoDocumento = $('#txtTipoIdentificacion')
+        this.$inputNroDocumento = $('#txtNroDocumento')
+        this.$inputEmail = $('#Email')
+        this.$inputTelefono = $('#Telefono')
+        this.$inputNombres = $('#txtNombres')
+        this.$inputApellidos = $('#txtApellidos')
+        this.$btnGenerarRecibo = $('#btnGenerarRecibo')
+    }
+
+    _initConstants() {       
+        this.API_POST_GUARDAR_PREINSCRIPTOS = '/Preinscripcion/SavePreinscriptos'
+        this.FORM_PREINSCRIPCION = 'form_preinscripcion'
+    }
+
+    _initEventBindings() {
+        this.$btnGenerarRecibo.on('click', this.clickHandlerGuardarPreinscritos)
+    }
+
+
+    _init() {
+       
+
+    }
+ 
+    postSavePreinscripto()
+    {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: this.API_POST_GUARDAR_PREINSCRIPTOS,
+                content: "application/json; charset=utf-8",
+                type: 'POST',
+                dataType: "json",
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                    resolve(data)
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    reject(new Error(`${errorThrown} - ${this.API_POST_GUARDAR_PREINSCRIPTOS}`))
+                }
+            })
+        })
+    }
+
+    async clickHandlerGuardarPreinscritos()
+    {
+        await this.postSavePreinscripto()
+    }
+
+}

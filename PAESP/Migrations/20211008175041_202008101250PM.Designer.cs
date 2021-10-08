9880656924f6b5862b4d5e76bf4967f8133940ed
Ivan J <iejimenez01@gmail.com>
2021-10-08 12:51:15 -0500

    migracion de correcion

--- PAESP/Migrations/20211008175041_202008101250PM.Designer.cs
+++ PAESP/Migrations/20211008175041_202008101250PM.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PAESP.Datos;
 
 namespace PAESP.Migrations
 {
     [DbContext(typeof(PaespDbContext))]
-    partial class PaespDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211008175041_202008101250PM")]
+    partial class _202008101250PM
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PAESP/Migrations/20211008175041_202008101250PM.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PAESP.Migrations
+{
+    public partial class _202008101250PM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Programa_Estudiantes_EstudianteIdEstudiante",
+                table: "Programa");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recibos_Conceptos_ConceptoIdConcepto",
+                table: "Recibos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recibos_ConceptoIdConcepto",
+                table: "Recibos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Programa_EstudianteIdEstudiante",
+                table: "Programa");
+
+            migrationBuilder.DropColumn(
+                name: "ConceptoIdConcepto",
+                table: "Recibos");
+
+            migrationBuilder.DropColumn(
+                name: "EstudianteIdEstudiante",
+                table: "Programa");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recibos_IdConcepto",
+                table: "Recibos",
+                column: "IdConcepto");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recibos_Conceptos_IdConcepto",
+                table: "Recibos",
+                column: "IdConcepto",
+                principalTable: "Conceptos",
+                principalColumn: "IdConcepto",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recibos_Conceptos_IdConcepto",
+                table: "Recibos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recibos_IdConcepto",
+                table: "Recibos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ConceptoIdConcepto",
+                table: "Recibos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EstudianteIdEstudiante",
+                table: "Programa",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recibos_ConceptoIdConcepto",
+                table: "Recibos",
+                column: "ConceptoIdConcepto");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Programa_EstudianteIdEstudiante",
+                table: "Programa",
+                column: "EstudianteIdEstudiante");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Programa_Estudiantes_EstudianteIdEstudiante",
+                table: "Programa",
+                column: "EstudianteIdEstudiante",
+                principalTable: "Estudiantes",
+                principalColumn: "IdEstudiante",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recibos_Conceptos_ConceptoIdConcepto",
+                table: "Recibos",
+                column: "ConceptoIdConcepto",
+                principalTable: "Conceptos",
+                principalColumn: "IdConcepto",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
